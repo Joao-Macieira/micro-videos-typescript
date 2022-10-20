a8b8aad01fcc67bb097e4847c3c3fe8594ca1b21
João Macieira <jp.macieira@uol.com.br>
2022-10-20 17:50:57 -0300

    feat: create to string function and test

--- src/@shared/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@shared/domain/value-objects/__tests__/value-object.spec.ts
@@ -9,5 +9,28 @@ describe("Value objects ubit tests", () => {
 
     vo = new StubValueObject({ props1: 'value1' });
     expect(vo.value).toStrictEqual({ props1: 'value1' });
-  })
+  });
+
+  it("sould convert to string", () => {
+    const date = new Date();
+
+    const arrange = [
+      { received: null, expected: "null" },
+      { received: undefined, expected: "undefined" },
+      { received: "", expected: "" },
+      { received: "fake test", expected: "fake test" },
+      { received: 0, expected: "0" },
+      { received: 1, expected: "1" },
+      { received: 5, expected: "5" },
+      { received: true, expected: "true" },
+      { received: false, expected: "false" },
+      { received: date, expected: date.toString() },
+      { received: { prop: 'value' }, expected: JSON.stringify({ prop: 'value' }) },
+    ];
+
+    arrange.forEach((value) => {
+      const vo = new StubValueObject(value.received);
+      expect(`${vo}`).toBe(value.expected);
+    });
+  });
 });
