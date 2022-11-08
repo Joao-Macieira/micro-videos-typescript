fe660e73ae92b36b24da7d6bbbd14362c31121a6
João Macieira <jp.macieira@uol.com.br>
2022-11-08 19:22:23 -0300

    feat: create in-memory category repository

---
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -0,0 +1,7 @@
+import { Category } from "../../domain/entities/category";
+import { InMemorySearchableRepository } from "../../../@shared/domain/repository/in-memory.repository";
+import CategoryRepository from "category/domain/repository/category.repository";
+
+export default class CategoryInMemoryRepository
+  extends InMemorySearchableRepository<Category>
+  implements CategoryRepository {}
