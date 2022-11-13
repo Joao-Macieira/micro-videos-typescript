626195f2f37b678c83aec98e15fa5cdb9a1e7c04
João Macieira <jp.macieira@uol.com.br>
2022-11-13 12:00:42 -0300

    feat: implements applySort in category in memory repository

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,10 +1,12 @@
 import { Category } from "../../domain/entities/category";
 import { InMemorySearchableRepository } from "../../../@shared/domain/repository/in-memory.repository";
 import CategoryRepository from "category/domain/repository/category.repository";
+import { SortDirection } from "../../../@shared/domain/repository/repository.contracts";
 
 export default class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
   implements CategoryRepository.Repository {
+    sortableFields: string[] = ['name', 'created_at'];
 
   protected async applyFilter(
     items: Category[],
@@ -18,5 +20,13 @@ export default class CategoryInMemoryRepository
       (item) =>
         item.props.name.toLowerCase().includes(filter.toLowerCase())
     );
+  }
+
+  protected async applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ): Promise<Category[]> {
+    return !sort ? super.applySort(items, "created_at", "desc") : super.applySort(items, sort, sort_dir)
   }
 }
