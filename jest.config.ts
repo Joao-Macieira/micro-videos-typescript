278af83403333e9623b1e05eecda21fab08fe475
João Macieira <jp.macieira@uol.com.br>
2023-01-21 14:57:11 -0300

    chore: configure test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -7,4 +7,13 @@ export default {
     '^.+\\.ts?$': ['@swc/jest'],
   },
   setupFilesAfterEnv: ["./@shared/domain/tests/validations.ts"],
+  coverageDirectory: '<rootDir>/../__coverage',
+  coverageThreshold: {
+    global: {
+      statements : 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
+  }
 };
