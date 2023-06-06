183e70c0d5ba65b6488a49215974b199b25572be
João Macieira <jp.macieira@uol.com.br>
2023-06-06 10:29:01 -0300

    feat: bulk create method

--- src/@core/src/@seedwork/infra/db/sequelize/sequelize.model-factory.ts
+++ src/@core/src/@seedwork/infra/db/sequelize/sequelize.model-factory.ts
@@ -1,14 +1,27 @@
 export class SequelizeModelFactory {
-  constructor(private model, private factoryProps: () => any) {}
+  private _count = 1;
 
-  async create(data?: any) {
-    return this.model.create(data ? data : this.factoryProps());
+  constructor(private model, private defaultFactoryProps: () => any) {}
+
+  count(count: number) {
+    this._count = count;
+    return this;
   }
 
-  async bulkCreate() {}
+  async create(data?: any) {
+    return this.model.create(data ? data : this.defaultFactoryProps());
+  }
+
+  async bulkCreate(factoryProps?: (index: number) => any) {
+    const data = new Array(this._count)
+      .fill(factoryProps ? factoryProps : this.defaultFactoryProps)
+      .map((factory, index) => factory(index));
+
+    return this.model.bulkCreate(data);
+  }
   
   make(data?: any) {
-    return this.model.build(data ? data : this.factoryProps());
+    return this.model.build(data ? data : this.defaultFactoryProps());
   }
 
   bulkMake() {}
