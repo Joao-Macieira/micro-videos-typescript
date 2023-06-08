1e24cf56820e435aa8ab181fd818f559b7e8b5d1
João Macieira <jp.macieira@uol.com.br>
2023-06-08 14:43:01 -0300

    feat: get category usecase integration test

---
+++ src/@core/src/category/application/use-cases/__tests__/integration/get-category.use-case.int-spec.ts
@@ -0,0 +1,35 @@
+import { CategorySequelize } from "#category/infra/db/sequelize/category-sequelize";
+import NotFoundError from "#seedwork/domain/errors/not-found.error";
+import { setupSequelize } from "#seedwork/infra/db/testing/helpers/db";
+import { GetCategoryUseCase } from "../../get-category.use-case";
+
+const { CategoryRepository, CategoryModel } = CategorySequelize;
+
+describe('GetCategoryUseCase integration tests', () => {
+  let useCase: GetCategoryUseCase.UseCase;
+  let repository: CategorySequelize.CategoryRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategoryRepository(CategoryModel);
+    useCase = new GetCategoryUseCase.UseCase(repository);
+  });
+
+  it('should throws error when entity not found', async () => {
+    await expect(useCase.execute({ id: 'fake id' })).rejects.toThrow(new NotFoundError('Entity not found using ID fake id'));
+  });
+
+  it('should returns a category', async () => {
+    const model = await CategoryModel.factory().create();
+    const output = await useCase.execute({ id: model.id });
+
+    expect(output).toStrictEqual({
+      id: model.id,
+      name: model.name,
+      description: model.description,
+      is_active: model.is_active,
+      created_at: model.created_at
+    })
+  });
+});
