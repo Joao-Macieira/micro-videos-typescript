c23f3806823696054146c9a913858f347c983b34
João Macieira <jp.macieira@uol.com.br>
2023-06-26 18:32:39 -0300

    feat: create category integration tests

--- src/nestjs/src/categories/tests/integration/categories.controller.int-spec.ts
+++ src/nestjs/src/categories/tests/integration/categories.controller.int-spec.ts
@@ -1,8 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Test, TestingModule } from '@nestjs/testing';
-import { CategoriesController } from '../../categories.controller';
-import { ConfigModule } from '../../../config/config.module';
-import { DatabaseModule } from '../../../database/database.module';
-import { CategoriesModule } from '../../../categories/categories.module';
 import {
   CreateCategoryUseCase,
   DeleteCategoryUseCase,
@@ -10,9 +7,16 @@ import {
   ListCategoriesUseCase,
   UpdateCategoryUseCase,
 } from '@core/micro-videos/category/application';
+import { CategoriesController } from '../../categories.controller';
+import { ConfigModule } from '../../../config/config.module';
+import { DatabaseModule } from '../../../database/database.module';
+import { CategoriesModule } from '../../../categories/categories.module';
+import { CATEGORY_PROVIDERS } from '../../category.providers';
+import { CategoryRepository } from '@core/micro-videos/category/domain';
 
 describe('CategoriesController integration tests', () => {
   let categoriesController: CategoriesController;
+  let repository: CategoryRepository.Repository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,6 +24,9 @@ describe('CategoriesController integration tests', () => {
     }).compile();
 
     categoriesController = module.get(CategoriesController);
+    repository = module.get(
+      CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+    );
   });
 
   it('should be defined', () => {
@@ -38,6 +45,90 @@ describe('CategoriesController integration tests', () => {
     );
     expect(categoriesController['deleteCategoryUseCase']).toBeInstanceOf(
       DeleteCategoryUseCase.UseCase,
+    );
+  });
+
+  describe('should create category', () => {
+    const arrange = [
+      {
+        request: {
+          name: 'Movie',
+        },
+        expectedOutput: {
+          name: 'Movie',
+          description: null,
+          is_active: true,
+        },
+      },
+      {
+        request: {
+          name: 'Movie',
+          description: null,
+        },
+        expectedOutput: {
+          name: 'Movie',
+          description: null,
+          is_active: true,
+        },
+      },
+      {
+        request: {
+          name: 'Movie',
+          is_active: true,
+        },
+        expectedOutput: {
+          name: 'Movie',
+          description: null,
+          is_active: true,
+        },
+      },
+      {
+        request: {
+          name: 'Movie',
+          description: 'movie description',
+          is_active: true,
+        },
+        expectedOutput: {
+          name: 'Movie',
+          description: 'movie description',
+          is_active: true,
+        },
+      },
+      {
+        request: {
+          name: 'Movie',
+          description: 'movie description',
+          is_active: false,
+        },
+        expectedOutput: {
+          name: 'Movie',
+          description: 'movie description',
+          is_active: false,
+        },
+      },
+    ];
+
+    test.each(arrange)(
+      'with request $request',
+      async ({ request, expectedOutput }) => {
+        const output = await categoriesController.create(request);
+
+        const entity = await repository.findById(output.id);
+
+        expect(entity).toMatchObject({
+          id: output.id,
+          name: expectedOutput.name,
+          description: expectedOutput.description,
+          is_active: expectedOutput.is_active,
+          created_at: output.created_at,
+        });
+
+        expect(output.id).toBe(entity.id);
+        expect(output.name).toBe(expectedOutput.name);
+        expect(output.description).toBe(expectedOutput.description);
+        expect(output.is_active).toBe(expectedOutput.is_active);
+        expect(output.created_at).toStrictEqual(entity.created_at);
+      },
     );
   });
 });
