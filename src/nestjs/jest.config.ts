bcd6b3955239fd306436813410da8ac51f9ceaad
João Macieira <jp.macieira@uol.com.br>
2023-07-06 09:39:53 -0300

    fix: adjust eslint and jest configs on nest

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -4,7 +4,6 @@ export default {
     color: 'magentaBright',
   },
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.*\\..*spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': '@swc/jest',
@@ -15,13 +14,13 @@ export default {
   testEnvironment: 'node',
   moduleNameMapper: {
     '@core/micro\\-videos/(.*)$':
-      '<rootDir>/../../../node_modules/@core/micro-videos/dist/$1',
+      '<rootDir>/../../node_modules/@core/micro-videos/dist/$1',
     '#seedwork/(.*)$':
-       '<rootDir>/../../../node_modules/@core/micro-videos/dist/@seedwork/$1',
+       '<rootDir>/../../node_modules/@core/micro-videos/dist/@seedwork/$1',
      '#category/(.*)$':
-       '<rootDir>/../../../node_modules/@core/micro-videos/dist/category/$1',
+       '<rootDir>/../../node_modules/@core/micro-videos/dist/category/$1',
   },
-  setupFilesAfterEnv: ['../../@core/src/@seedwork/domain/tests/jest.ts'],
+  setupFilesAfterEnv: ['../@core/src/@seedwork/domain/tests/jest.ts'],
   coverageThreshold: {
     global: {
       statements: 80,
