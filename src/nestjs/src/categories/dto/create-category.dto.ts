b87a438d59aea2c1f179482ae5ca028f7f7b9ab3
João Macieira <jp.macieira@uol.com.br>
2023-07-07 15:07:15 -0300

    feat: validate category dtos

--- src/nestjs/src/categories/dto/create-category.dto.ts
+++ src/nestjs/src/categories/dto/create-category.dto.ts
@@ -1,9 +1,16 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional, IsBoolean } from 'class-validator';
 import { CreateCategoryUseCase } from '@core/micro-videos/category/application';
 
 export class CreateCategoryDto implements CreateCategoryUseCase.Input {
+  @IsString()
   @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsBoolean()
+  @IsOptional()
   is_active?: boolean;
 }
