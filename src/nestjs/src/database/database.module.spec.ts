228bde06a7471875d89e44a92d1332ed9d37e53f
João Macieira <jp.macieira@uol.com.br>
2023-07-12 14:26:39 -0300

    feat: test mysql connection

--- src/nestjs/src/database/database.module.spec.ts
+++ src/nestjs/src/database/database.module.spec.ts
@@ -51,10 +51,10 @@ describe('Database module tests', () => {
     const connectionOptions = {
       DB_VENDOR: 'mysql',
       DB_HOST: 'localhost',
-      DB_DATABASE: 'micro-videos',
+      DB_DATABASE: 'micro_videos_test',
       DB_USERNAME: 'root',
       DB_PASSWORD: 'root',
-      DB_PORT: 3306,
+      DB_PORT: 3307,
       DB_LOGGING: false,
       DB_AUTO_LOAD_MODELS: true,
     };
@@ -68,31 +68,31 @@ describe('Database module tests', () => {
       expect(error).toBeUndefined();
     });
 
-    // it('should be a mysql connection', async () => {
-    //   const module = await Test.createTestingModule({
-    //     imports: [
-    //       DatabaseModule,
-    //       ConfigModule.forRoot({
-    //         isGlobal: true,
-    //         ignoreEnvFile: true,
-    //         ignoreEnvVars: true,
-    //         validationSchema: null,
-    //         load: [() => connectionOptions],
-    //       }),
-    //     ],
-    //   }).compile();
+    it('should be a mysql connection', async () => {
+      const module = await Test.createTestingModule({
+        imports: [
+          DatabaseModule,
+          ConfigModule.forRoot({
+            isGlobal: true,
+            ignoreEnvFile: true,
+            ignoreEnvVars: true,
+            validationSchema: null,
+            load: [() => connectionOptions],
+          }),
+        ],
+      }).compile();
 
-    //   const app = module.createNestApplication();
-    //   const connection = app.get<Sequelize>(getConnectionToken());
+      const app = module.createNestApplication();
+      const connection = app.get<Sequelize>(getConnectionToken());
 
-    //   expect(connection).toBeDefined();
-    //   expect(connection.options.dialect).toBe('mysql');
-    //   expect(connection.options.host).toBe('localhost');
-    //   expect(connection.options.database).toBe('micro-videos');
-    //   expect(connection.options.username).toBe('root');
-    //   expect(connection.options.password).toBe('root');
-    //   expect(connection.options.port).toBe(3306);
-    //   await connection.close();
-    // });
+      expect(connection).toBeDefined();
+      expect(connection.options.dialect).toBe(connectionOptions.DB_VENDOR);
+      expect(connection.options.host).toBe(connectionOptions.DB_HOST);
+      expect(connection.options.database).toBe(connectionOptions.DB_DATABASE);
+      expect(connection.options.username).toBe(connectionOptions.DB_USERNAME);
+      expect(connection.options.password).toBe(connectionOptions.DB_PASSWORD);
+      expect(connection.options.port).toBe(connectionOptions.DB_PORT);
+      await connection.close();
+    });
   });
 });
