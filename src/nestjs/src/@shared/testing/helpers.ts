d48b016278eccdfd78da311ac0e258e2800fbfea
João Macieira <jp.macieira@uol.com.br>
2023-07-15 11:33:33 -0300

    feat: configure migrations

--- src/nestjs/src/@shared/testing/helpers.ts
+++ src/nestjs/src/@shared/testing/helpers.ts
@@ -1,7 +1,9 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getConnectionToken } from '@nestjs/sequelize';
+import { ConfigService } from '@nestjs/config';
 import { Sequelize } from 'sequelize';
+import { migrator } from '@core/micro-videos/@seedwork/infra';
 import { AppModule } from '../../app.module';
 import { applyGlobalConfig } from '../../global-config';
 
@@ -9,13 +11,32 @@ export function startApp({
   beforeInit,
 }: { beforeInit?: (app: INestApplication) => void } = {}) {
   let _app: INestApplication;
+  let canRunMigrations: boolean;
 
   beforeEach(async () => {
     const moduleBuilder: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+
+    canRunMigrations = !moduleBuilder
+      .get(ConfigService)
+      .get('DB_AUTO_LOAD_MODELS');
+
     const sequelize = moduleBuilder.get<Sequelize>(getConnectionToken());
-    await sequelize.sync({ force: true });
+
+    try {
+      if (canRunMigrations) {
+        const umzug = migrator(sequelize, { logger: undefined });
+        await sequelize.drop();
+        await umzug.up();
+      } else {
+        await sequelize.sync({ force: true });
+      }
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+
     _app = moduleBuilder.createNestApplication();
     applyGlobalConfig(_app);
     beforeInit && beforeInit(_app);
